17f3eb6c9015b6a8a2c60e2f16b5000ffcdc787d
ironnerves <ironnerves191@gmail.com>
2026-01-06 21:35:09 -0500

    Finished adding getaddrinfo to makeSocket.

--- makefile
+++ makefile
@@ -1,9 +1,9 @@
 .PHONY: run clean compile
 run: server
-	./server
+	@./server
 compile server: server.o
-	gcc server.o -Wall -o server
+	@gcc server.o -Wall -o server
 server.o: server.c
-	gcc -c server.c
+	@gcc -c server.c
 clean:
 	rm -f *.o *.gch server
--- server.c
+++ server.c
@@ -3,12 +3,24 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#define PORT "16532"
+void err() {
+  printf("Error: %s\n", strerror(errno));
+  exit(1);
+}
 int makeSocket() {
   struct addrinfo * serverInfo, * hints;
-  hints = (addrinfo *)calloc(1, sizeof(struct addrinfo));
+  hints = (struct addrinfo *)calloc(1, sizeof(struct addrinfo));
   hints->ai_family = AF_UNSPEC;
   hints->ai_socktype = SOCK_STREAM;
   hints->ai_flags = AI_PASSIVE;
+  if (getaddrinfo(NULL,PORT,hints, &serverInfo)) err();
+
+  freeaddrinfo(hints);
+  freeaddrinfo(serverInfo);
 }
 int main() {
 
