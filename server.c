df56cc2ef84b74a586f268c3230da4538739b3af
ironnerves <ironnerves191@gmail.com>
2026-01-06 22:16:30 -0500

    Added the creation of the client socket.

--- server.c
+++ server.c
@@ -12,7 +12,7 @@ void err() {
   printf("Error: %s\n", strerror(errno));
   exit(1);
 }
-int makeSocket() {
+int makeServerSocket() {
   struct addrinfo * serverInfo, * hints;
   hints = (struct addrinfo *)calloc(1, sizeof(struct addrinfo));
   hints->ai_family = AF_INET;
@@ -27,10 +27,22 @@ int makeSocket() {
   sockAddr = (struct sockaddr_in *)serverInfo->ai_addr;
   inet_ntop(serverInfo->ai_family, &(sockAddr->sin_addr),serverIP, 128);
   printf("Server Address: %s:%s\n", serverIP, PORT);
+  listen(serverSock,1);
   freeaddrinfo(hints);
   freeaddrinfo(serverInfo);
   return 0;
 }
+int makeClientSocket(int serverSock) {
+  struct sockaddr_storage clientAddr;
+  int clientSock;
+  int size = sizeof(struct sockaddr_storage);
+  clientSock = accept(serverSock,(struct sockaddr_storage *)&clientAddr,&clientSock);
+  printf("Connection Success!\n");
+  return clientSock;
+}
 int main() {
-  makeSocket();
+  int serverSock = makeServerSocket();
+  while (1) {
+      clientSock = makeClientSocket;
+  }
 }
