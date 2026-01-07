fe76e5c3b09667f5e51f5864288219b1556b3fab
ironnerves <ironnerves191@gmail.com>
2026-01-06 22:01:26 -0500

    Added printing of server IP, but it doesn't work (it registers itself as 0.0.0.0)

--- server.c
+++ server.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <errno.h>
 #include <stdio.h>
+#include <arpa/inet.h>
 #define PORT "16532"
 void err() {
   printf("Error: %s\n", strerror(errno));
@@ -14,7 +15,7 @@ void err() {
 int makeSocket() {
   struct addrinfo * serverInfo, * hints;
   hints = (struct addrinfo *)calloc(1, sizeof(struct addrinfo));
-  hints->ai_family = AF_UNSPEC;
+  hints->ai_family = AF_INET;
   hints->ai_socktype = SOCK_STREAM;
   hints->ai_flags = AI_PASSIVE;
   if (getaddrinfo(NULL,PORT,hints, &serverInfo)) err();
@@ -22,11 +23,14 @@ int makeSocket() {
   serverSock = socket(serverInfo->ai_family,serverInfo->ai_socktype,serverInfo->ai_protocol);
   bind(serverSock,serverInfo->ai_addr,serverInfo->ai_addrlen);
   char * serverIP = (char *)malloc(128);
-  inet_ntop(p_ai_family, )
-  printf("Server Address: %s:%s\n", )
+  struct sockaddr_in *sockAddr;
+  sockAddr = (struct sockaddr_in *)serverInfo->ai_addr;
+  inet_ntop(serverInfo->ai_family, &(sockAddr->sin_addr),serverIP, 128);
+  printf("Server Address: %s:%s\n", serverIP, PORT);
   freeaddrinfo(hints);
   freeaddrinfo(serverInfo);
+  return 0;
 }
 int main() {
-
+  makeSocket();
 }
